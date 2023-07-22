70dd4131fab19c963782920034aee6fc826524ea
safwaan41 <safwaan41@gmail.com>
2023-07-22 16:21:08 -0500

    added node and got server running

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require(`express`);
+const path = require(`path`);
+const db = require(`./db/db.json`);
+const fs = require(`fs`);
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.static(`public`));
